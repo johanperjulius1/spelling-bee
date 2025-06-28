5e42fea387b14527ad36710affd795cacbea7402
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-06-28 17:38:18 +0200

    starting refactoring of hex buttons

--- src/components/GameControls/HexButtons.jsx
+++ src/components/GameControls/HexButtons.jsx
@@ -23,53 +23,21 @@ export function HexButtons({ shuffledOuterLetters, setUserInput }) {
 
   return (
     <div className={styles["hive"]}>
+      <div className={styles["button-container"]}>{outerButtons[0]}</div>
       <div className={styles["button-container"]}>
+        {outerButtons[1]}
         <button
-          onClick={handleButtonClick}
-          className={`${styles["hex-button"]} ${styles["button-one"]}`}
-        >
-          {shuffledOuterLetters[0]}
-        </button>
-      </div>
-      <div className={styles["button-container"]}>
-        <button
-          onClick={handleButtonClick}
-          className={`${styles["hex-button"]} ${styles["button-two"]}`}
-        >
-          {shuffledOuterLetters[1]}
-        </button>
-        <button
-          onClick={handleButtonClick}
           className={`${styles["hex-button"]} ${styles["center"]}`}
-        >
-          {centerLetter}
-        </button>
-        <button
           onClick={handleButtonClick}
-          className={`${styles["hex-button"]} ${styles["button-three"]}`}
         >
-          {shuffledOuterLetters[2]}
+          {centerLetter?.toUpperCase() || ""}
         </button>
+        {outerButtons[2]}
       </div>
       <div className={styles["button-container"]}>
-        <button
-          onClick={handleButtonClick}
-          className={`${styles["hex-button"]} ${styles["button-four"]}`}
-        >
-          {shuffledOuterLetters[3]}
-        </button>
-        <button
-          onClick={handleButtonClick}
-          className={`${styles["hex-button"]} ${styles["button-five"]}`}
-        >
-          {shuffledOuterLetters[4]}
-        </button>
-        <button
-          onClick={handleButtonClick}
-          className={`${styles["hex-button"]} ${styles["button-six"]}`}
-        >
-          {shuffledOuterLetters[5]}
-        </button>
+        {outerButtons[3]}
+        {outerButtons[4]}
+        {outerButtons[5]}
       </div>
     </div>
   );
